67c0eb9bc3f909ee9b590172045cd138de82214a
DanielGR <jdanielgr2005@gmail.com>
2026-02-11 12:13:43 -0800

    update recommendedCarrousel

--- src/components/RecommendedCarousel.tsx
+++ src/components/RecommendedCarousel.tsx
@@ -37,7 +37,7 @@ export default function RecommendedCarousel(){
 
             <div ref = {carouselRef} 
                 className="flex gap-4 overflow-x-auto scroll-smooth px-10 h-60">
-                {products.map((product:product) => {
+                {products?.map((product:product) => {
                     if(!product.available || !product.recommended) return;
                     return(
                     <div key={product.id}  className="min-w-[250px] bg-white rounded-xl shadow">
